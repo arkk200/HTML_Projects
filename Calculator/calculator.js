b00169f7f6b31b8ea45bab9384d3ac83faec4342
arkk200 <arkk20000@gmail.com>
2022-06-16 16:43:38 +0900

    2022-06-16, 04:42(PM), Add Clear Entity button and somethings more v3

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,30 +1,52 @@
 const onClickNumber = (event) => () => {
     if(op[count]) {count++;}
-    if(setInitial) {value = []; $result.value = ''; setInitial = false;}
-    if(value[count] == undefined) { value[count] = ''; }
+    if(setInitial) {value = []; result = ''; setInitial = false;}
+    if(value[count] == undefined) {value[count] = '';} // value[count]가 undefined이면 ''값으로 초기화
+    if(value[count] === 0) result = result.slice(0, -1); // value[count]가 0일 때 값이 입력 되면 result 뒤에 0 제거
+    // 0이 입력된다면 화면에 0 하나만 표시한다.
+    if(!value[count] && event == 0) {console.log("a"); value[count] = 0; result += 0; $result.value = result; return;}
     value[count] += event;
-    $result.value += event;
+    result += event;
+    $result.value = result;
 };
 const onClickCal = (event) => () => {
-    if(value[count]){
+    if(value[count] || value[count] === 0){
+        console.log()
+        if(!!op[count]) result = result.slice(0, -1);
         op[count] = event;
-        $result.value += event;
+        result += event;
+        $result.value = result;
         setInitial = false;
     }
 };
 const Clear = () => {
     num = 0;
+    result = '';
     count = 0;
     value = [];
     op = [];
     $result.value = '';
 };
 
+const ClearEntry = () => {
+    if(op[count]){
+        op[count] = '';
+        result = result.slice(0, -1);
+    }
+    else if(value[count] || value[count] == 0){
+        for(let i = 0; i < String(value[count]).length; i++){
+            result = result.slice(0, -1);
+        }
+        value[count] = '';
+        if(!!count) count--;
+    }
+    $result.value = result;
+}
+
 const showResult = () => {
-    if(!op[count] && value[count]){
+    if(!op[count] && (value[count] || value[count] === 0)){
         console.log(value);
         console.log(op);
-        console.log(!!op[0]);
         for(let i = 0; op[i]; i++){
             if(op[i] == '*'){
                 console.log(value[i], op[i], value[i+1]);
@@ -60,7 +82,7 @@ const showResult = () => {
             }
         }
         $result.value = value[0];
-        console.log(value[0]);
+        result = String(value[0]);
         num = 0;
         count = 0;
         op = [];
@@ -78,6 +100,8 @@ document.querySelector('#seven').addEventListener('click', onClickNumber('7'));
 document.querySelector('#eight').addEventListener('click', onClickNumber('8'));
 document.querySelector('#nine').addEventListener('click', onClickNumber('9'));
 document.querySelector('#zero').addEventListener('click', onClickNumber('0'));
+document.querySelector('#doubleZero').addEventListener('click', onClickNumber('00'));
+document.querySelector('#decimalPoint').addEventListener('click', onClickNumber('.'));
 
 document.querySelector('#plus').addEventListener('click', onClickCal('+'));
 document.querySelector('#minus').addEventListener('click', onClickCal('-'));
@@ -86,13 +110,14 @@ document.querySelector('#multiple').addEventListener('click', onClickCal('*'));
 
 document.querySelector('#is').addEventListener('click', showResult);
 document.querySelector('#clear').addEventListener('click', Clear);
+document.querySelector('#clearEntry').addEventListener('click', ClearEntry);
 $result = document.querySelector('#result');
 $is = document.querySelector('#is');
 
 let num;
+let result = '';
 let count = 0;
 let value = [];
 let op = [];
-let result;
 let setInitial = false;
 $is.addEventListener('click', showResult);
